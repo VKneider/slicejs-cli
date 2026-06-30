af946f8c1f2583edb9c7b03674f05a1f344a269b
VKneider <victorkneider@gmail.com>
2026-06-29 23:48:48 -0400

    fix: disable Terser property mangling to prevent vendor library breakage
    The { regex: /^_/ } option mangled underscore-prefixed properties
    (e.g. _rep) but left string literals unchanged, causing runtime
    errors in libraries like anime.js that use dynamic string lookups.
    
    Setting properties: false eliminates this class of bugs entirely.
    Code is still minified and variable/function names are mangled.

--- commands/utils/bundling/BundleGenerator.js
+++ commands/utils/bundling/BundleGenerator.js
@@ -963,7 +963,7 @@ export default class BundleGenerator {
         passes: 1
       } : false,
       mangle: this.options.obfuscate ? {
-        properties: { regex: /^_/ }
+        properties: false
       } : false,
       keep_fnames: true,
       keep_classnames: true,
